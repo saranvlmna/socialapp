a691fa765e866e58325379076582d5e6684f3125
Saranjith <saranvlmna@gmail.com>
2025-02-05 15:08:22 +0530

    update user table schema and add posts table migration

--- alembic/versions/dfc81064b9fb_table_post.py
+++ alembic/versions/dfc81064b9fb_table_post.py
@@ -2,24 +2,24 @@ from alembic import op
 import sqlalchemy as sa
 
 # Revision identifiers, used by Alembic.
-revision = '92b6bada9837'  # Change this to match your file's revision ID
+revision = 'dfc81064b9fb'  # Change this to match your file's revision ID
 down_revision = None  # Set this to the previous migration if applicable
 branch_labels = None
 depends_on = None
 
 def upgrade():
-    """Apply the migration: Create users table."""
+    """Apply the migration: Create posts table."""
     op.create_table(
-        'users',
+        'posts',
         sa.Column('id', sa.Uuid, primary_key=True, default=sa.func.uuid_generate_v4()),
-        sa.Column('name', sa.String(), nullable=False),
-        sa.Column('email', sa.String(), unique=True, nullable=False),
-        sa.Column('age', sa.Numeric(), nullable=False),
-        sa.Column('password', sa.String(), nullable=False),
+        sa.Column('user_id', sa.String(), unique=True, nullable=False),
+        sa.Column('title', sa.String(), nullable=False),
+        sa.Column('discription', sa.String(), nullable=False),
+        sa.Column('url', sa.String(), nullable=False),
         sa.Column('created_at', sa.DateTime, server_default=sa.func.now()),
         sa.Column('updated_at', sa.DateTime, server_default=sa.func.now(), onupdate=sa.func.now()),
     )
 
 def downgrade():
-    """Rollback the migration: Drop users table."""
-    op.drop_table('users')
+    """Rollback the migration: Drop posts table."""
+    op.drop_table('posts')
